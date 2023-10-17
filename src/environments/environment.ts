ed390e5498d3b468d4e711a12af5212778184e7a
Blessed Tasela <taselablessed@gmail.com>
2023-10-17 14:48:42 +0100

    added ip config to allow local networks

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
-export const environment ={
-production: false,
-apiUrl: 'http://localhost:8001'
-};
+export const environment = {
+    production: false,
+    apiUrl: 'http://192.168.0.102:8001' // Update this with the correct IP address and port
+  };
+  
