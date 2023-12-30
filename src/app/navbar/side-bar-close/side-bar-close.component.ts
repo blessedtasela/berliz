f3dc726841cd214411815768ab3787fc4c6bcfcf
Blessed Tasela <taselablessed@gmail.com>
2023-12-30 03:35:09 +0100

    added side-bar-closed menu for medium screen

--- src/app/navbar/side-bar-close/side-bar-close.component.ts
+++ src/app/navbar/side-bar-close/side-bar-close.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Router, NavigationEnd } from '@angular/router';
+import { Users } from 'src/app/models/users.interface';
+import { SnackBarService } from 'src/app/services/snack-bar.service';
+import { UserService } from 'src/app/services/user.service';
+import { PromptModalComponent } from 'src/app/shared/prompt-modal/prompt-modal.component';
 
 @Component({
   selector: 'app-side-bar-close',
@@ -6,5 +12,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-bar-close.component.css']
 })
 export class SideBarCloseComponent {
+  currentRoute: any;
+  responseMessage: any;
+  @Input() userData!: Users;
+  currentRouteName: string | null = null;
 
+  constructor(private router: Router,
+    private dialog: MatDialog,
+    private userService: UserService,
+    private snackbarService: SnackBarService) {
+    this.currentRoute = this.router.url
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+      }
+    });
+  }
+
+  isActive(path: string): boolean {
+    return this.currentRoute?.startsWith('/' + path);
+  }
+
+  isPath(path: string): boolean {
+    return this.currentRoute === '/' + path;;
+  }
+
+  setRouterName(routeName: string) {
+    this.currentRouteName = routeName;
+  }
+
+  clearRouterName(): void {
+    this.currentRouteName = null;
+  }
+
+  logout() {
+    console.log('logging out')
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      message: 'Logout',
+      confirmation: true
+    };
+    const dialogRef = this.dialog.open(PromptModalComponent, dialogConfig);
+    const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe((response: any) => {
+      dialogRef.close();
+      this.userService.logout();
+      this.responseMessage = "you have successfully logged out"
+      this.snackbarService.openSnackBar(this.responseMessage, '');
+    });
+  }
 }
--- src/app/navbar/side-bar-open/side-bar-open.component.ts
+++ src/app/navbar/side-bar-open/side-bar-open.component.ts
@@ -12,43 +12,4 @@ import { PromptModalComponent } from 'src/app/shared/prompt-modal/prompt-modal.c
   styleUrls: ['./side-bar-open.component.css']
 })
 export class SideBarOpenComponent {
-  currentRoute: any;
-  responseMessage: any;
-  @Input() userData!: Users;
-
-  constructor(private router: Router,
-    private dialog: MatDialog,
-    private userService: UserService,
-    private snackbarService: SnackBarService) {
-    this.currentRoute = this.router.url
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.currentRoute = event.url;
-      }
-    });
-  }
-
-  isActive(path: string): boolean {
-    return this.currentRoute?.startsWith('/' + path);
-  }
-
-  isPath(path: string): boolean {
-    return this.currentRoute === '/' + path;;
-  }
-
-  logout() {
-    console.log('logging out')
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.data = {
-      message: 'Logout',
-      confirmation: true
-    };
-    const dialogRef = this.dialog.open(PromptModalComponent, dialogConfig);
-    const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe((response: any) => {
-      dialogRef.close();
-      this.userService.logout();
-      this.responseMessage = "you have successfully logged out"
-      this.snackbarService.openSnackBar(this.responseMessage, '');
-    });
-  }
 }
