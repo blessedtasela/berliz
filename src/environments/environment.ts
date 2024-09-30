864f3ae2c8d49065da7d50c4a253f5adbd1fbdcb
Blessed <taselablessed@gmail.com>
2024-09-29 22:50:20 -0700

    added domain name to berliz

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,13 @@
+export const berlizUrl = 'https://www.berliz.fitness';
+
 export const environment = {
     production: false,
     // apiUrl: 'https://berliz-server-fd9efef771e8.herokuapp.com',
     apiUrl: 'http://localhost:8001',
     // apiUrl: 'http://192.168.43.56:8001',
 
-    baseUrl: 'https://berliz.netlify.app',
-    assetsUrl: 'https://berliz.netlify.app/assets/berliz-files/',
+    baseUrl: berlizUrl,
+    assetsUrl: `${berlizUrl}/assets/berliz-files/`,
     socialUrls: {
         facebook: 'https://www.facebook.com/berlizfitness',
         instagram: 'https://www.instagram.com/berlizfitness',
@@ -15,3 +17,5 @@ export const environment = {
     brokerURL: 'ws://localhost:8001/stomp',
     //  brokerURL : 'ws://192.168.43.56:8001/stomp',
 };
+
+
